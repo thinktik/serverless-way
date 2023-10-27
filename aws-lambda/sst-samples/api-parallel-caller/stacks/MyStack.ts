2bb2ace9d9e2b05fb14f81689b067906cd0c7ba2
thinktik <thinktik@outlook.com>
2023-10-27 13:19:04 +0800

    1. 补充Amazon MQ和Lambda的集成

--- aws-lambda/sst-samples/api-parallel-caller/stacks/MyStack.ts
+++ aws-lambda/sst-samples/api-parallel-caller/stacks/MyStack.ts
@@ -3,43 +3,45 @@ import {LambdaInvoke} from "aws-cdk-lib/aws-stepfunctions-tasks";
 import {Chain, JsonPath, Map, StateMachine} from "aws-cdk-lib/aws-stepfunctions";
 
 export function MyStack({stack}: StackContext) {
-    //The creation of the first Step Function that will use the handler located in packages/functions/src/callYoutubeAPi
+    // Step Function的第二个任务，用于模拟获取压测任务的参数
     const getApiInfo = new LambdaInvoke(stack, "getApiInfo", {
         lambdaFunction: new Function(stack, "getApiInfo-func", {
             handler: "packages/functions/src/getApiInfo.handler",
         }),
     });
 
+    // Step Function的第三个任务，用于模拟执行压测任务
     const callApiParallel = new LambdaInvoke(stack, "callApiParallel", {
         lambdaFunction: new Function(stack, "callApiParallel-func", {
             handler: "packages/functions/src/callApiParallel.handler",
         }),
     });
 
+    // Step Function的第四个任务，用于模拟执行压测任务完成后的数据统计
     const statisticCalledResults = new LambdaInvoke(stack, "statisticCalledResults", {
         lambdaFunction: new Function(stack, "statisticCalledResults-func", {
             handler: "packages/functions/src/statisticCalledResults.handler",
         }),
     });
 
-
+    // Step Function管理，创建一个Map状态机并行执行的任务，最大并发50
     const map = new Map(stack, "MapCompute", {
         maxConcurrency: 50,
         inputPath: JsonPath.stringAt('$.Payload'),
         itemsPath: JsonPath.stringAt('$.callApis')
     });
 
-    //The creation of the chain of states
+    // 创建一个step function的state链，串联前面的map任务和最后的统计任务
     const stateDefinition = Chain.start(getApiInfo)
         .next(map.iterator(callApiParallel))
         .next(statisticCalledResults);
 
-    //The creation of the state machine
+    // 用stateDefinition定义Step function的逻辑
     const stateMachine = new StateMachine(stack, "ApiParallelCallerStepMachine", {
         definition: stateDefinition,
     });
 
-    //The creation of the API with a GET route "/start-machine" that will call the handler located in packages/functions/src/startMachine
+    // Step Function的第一个任务，用于指示Step Function开始运行，当用户访问/start-machine时触发整个step function的启动
     const api = new Api(stack, "apiStartMachine", {
         routes: {
             "GET /start-machine": {
@@ -53,7 +55,7 @@ export function MyStack({stack}: StackContext) {
         },
     });
 
-    //To grant the permission to our route to start our state machine
+    // To grant the permission to our route to start our state machine
     api.attachPermissionsToRoute("GET /start-machine", [
         [stateMachine, "grantStartExecution"],
     ]);
